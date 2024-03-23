b8a5be28a2708a85c14234d2dbb04e1c5fa35479
zBsX <155221607+zBsX@users.noreply.github.com>
2024-03-23 10:27:18 +0800

    Final Test
    second update

--- studentid.py
+++ studentid.py
@@ -1,6 +1,7 @@
 import json
 import re
 import os
+# import tkinter
 
 class Student:
     def __init__(self, student_id=None, student_name=None, student_age=None, student_gender=None, student_score=None):
@@ -48,7 +49,7 @@ class Student:
                     print('学生列表:', data)
                     continue
                 if self.student_id == 0:
-                    print('abort')
+                    print('Abort')
                     break
                 print('你的id是:', self.student_id)
                 
@@ -62,8 +63,8 @@ class Student:
                             print('请输入正确的名字')
                             continue
                         break
-                    except ValueError as e:
-                        print(e)
+                    except ValueError as Error:
+                        print(Error)
                         continue
                 
                 print('你的名字是' , self.student_name)
@@ -83,13 +84,24 @@ class Student:
                 if self.student_gender == '退出':
                     break
                 print('你的性别是' , self.student_gender)
+                
                 while True:
                     try:
+                        
                         self.student_score = float(input('score (-1 to quit):'))
-                        if self.student_gender == -1:
-                            break
-                        print('你的分数是' , self.student_score)
-                        break
+                        match self.student_score:
+                            case self.student_score if self.student_score < -1:
+                                print('不能小于0')
+                                continue
+                            case self.student_score if self.student_score > 750:
+                                print('不能大于750')
+                                continue
+                            case _:
+                                if self.student_score == -1:
+                                    break
+                                print('你的分数是' , self.student_score)
+                                break
+                        
                     except ValueError as e:
                         print(e)
                         continue    
@@ -116,15 +128,16 @@ class Student:
         # print(self.student_dict)
         
     def select_student(self):
+        idset = set()
         while True:
             for idx,id in enumerate(self.sid, 1):
                 print(f'{idx},{id}')
             
             with open('data.json', 'r', encoding='utf-8') as file:
-                data2 = json.load(file)
-            # print('data2:', data2)
+                student_data_dict = json.load(file)
+            # print('student_data_dict:', student_data_dict)
             
-            matches = re.findall(self.patterns, str(data2))
+            matches = re.findall(self.patterns, str(student_data_dict))
             self.sid = [int(i) for i in matches[::3]]
             print('学生id:', self.sid)
 
@@ -132,25 +145,48 @@ class Student:
                 print('目前没有学生')
                 exit()
             try:
-                selected_idx = int(input("Enter the number of the student id you want to check (0 to exit, -1 to read data): "))
-                # if-else statement
-                if selected_idx > len(self.sid) or selected_idx < -1:
-                    print('超出范围，请重试')
-                    continue
-                if selected_idx == 0:
-                    break
-                elif selected_idx == -1:
-                    print(data2)
+                selected_id = int(input('请输入想要查询的ID(按0退出):'))
+                
+                for id in self.sid:
+                    idset.add(id)
+                # print(idset, student_data_dict)
+                if selected_id in idset:
+                    print(f'学生信息:{student_data_dict[str(selected_id)]}')
+                elif selected_id == 0:
+                    exit()
                 else:
-                    selected_id = self.sid[selected_idx - 1]
-                    print(data2[str(selected_id)])
+                    print('未找到该学生')
+                
+                
+                
+                
+                
+                # selected_idx = int(input("Enter the number of the student id you want to check (0 to exit, -1 to read data): "))
+                # # if-else statement
+                # if selected_idx > len(self.sid) or selected_idx < -1:
+                #     print('超出范围，请重试')
+                #     continue
+                # if selected_idx == 0:
+                #     break
+                # elif selected_idx == -1:
+                #     print(student_data_dict)
+                # else:
+                    # selected_id = self.sid[selected_idx - 1]
+                    # print(student_data_dict[str(selected_id)])
+                    # selected_id = 
+                    
                 self.sid.clear()
                 
             except ValueError:
                 print('无效输入')
                 continue
+            
 if __name__ == '__main__':                  
-    null = False
-    student = Student(null, null, null, null, null)
-    student.inputid()
-    student.select_student()
+    self = Student()
+    selection = input('请选择创建还是查找:')
+    if '创建' in selection:
+        Student.inputid(self)
+    elif '查找' in selection:
+        Student.select_student(self)
+    else:
+        print('输入错误')
