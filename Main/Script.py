626397d140c42375d27a6036406272dbd0ef54c8
zBsX <155221607+zBsX@users.noreply.github.com>
2024-05-05 22:53:28 +0800

    Update JSON file saving
    update 2024/5/5

--- Main/Script.py
+++ Main/Script.py
@@ -1,21 +1,21 @@
 from tkinter import ttk
 from tkinter import *
-from printinfo import *
 import json
 
 Tasks_dict = dict()
 counter = 0
+sign_set = set()
 d_conuter = 0
 Label_list = []
 Total_Tasks = ""
 status = [False, False]
 details = {}
-TASKS_HEIGH = 30
+TASKS_HEIGH = 111
 TASKS_ITEMS = 3
 Global_height = 1
 LIST_HEIGHT = TASKS_HEIGH * TASKS_ITEMS
 MAINFRAME_WIDGETS_HEIGHT = 54
-
+Tempinfo = []
 
 
 ### Main UI
@@ -47,7 +47,14 @@ class UI:
         self.new_frame = Frame(self.canvas, bg="green")
         self.Add_Tasks()
         print("Load_counter:", counter)
-        self.Resize(event=None)
+        self.canvas.create_window(
+                (0, 0), 
+                window=self.new_frame, 
+                anchor="nw", 
+                width=381, 
+                height=Global_height
+            )
+        
 
         # Slider 
         self.slider = ttk.Scrollbar(self.canvas, orient="vertical", command=self.canvas.yview)
@@ -89,6 +96,7 @@ class UI:
         global details
         global d_conuter
         global counter
+        global sign_set
         global Global_height
 
         # add task height
@@ -111,7 +119,9 @@ class UI:
 
         # Main Title
         
-       
+        sign_set.add(counter)
+        print(" ok button sign_set:", sign_set)
+        sign = list(sign_set)
 
         t = ttk.Label(self.new_frame, text=Tasks_dict[f"Task_{counter}"], font=("", 20))
         t.pack(anchor="w")
@@ -123,12 +133,15 @@ class UI:
 
 
         # Finished Button
-        b = ttk.Button(self.new_frame, text="Finished", command=lambda: self.Finished(t, t2, b))
+        b = ttk.Button(self.new_frame, text=f"Finished{sign}", command=lambda: self.Finished(t, t2, b, sign_set))
         b.pack(anchor="e")
 
         # global counter
         counter += 1
         d_conuter += 1
+        # add to sign set
+        sign_set.add(counter)
+        print("sign_set:", sign_set)
         print("Counter:", counter)
         print("d_counter", d_conuter)
         # print(details)
@@ -140,8 +153,10 @@ class UI:
         return
 
 
-    def Finished(self, text, text2, button):
+    def Finished(self, text, text2, button, sign_set):
         global status
+
+        print(sign_set)
 
         # Comfirm Window
         if status[1] == False:
@@ -164,12 +179,17 @@ class UI:
     def Destroy_Script(self, window, member, text, text2, button):
         global Global_height
         
+        
         # subtract by task height
         Global_height -= LIST_HEIGHT
         print("global height:", Global_height)
 
-        self.Resize(event=None)
-        self.update_canvas_height()
+        # Scripts
+        # self.Resize(event=None)
+        # self.update_canvas_height()
+        self.ImportJSON()
+        self.DeleteJSON()
+
 
 
         # destroy things
@@ -227,20 +247,15 @@ class UI:
 
                 self.canvas.unbind_all("<MouseWheel>")
 
-                # printinfo_lesser(Global_height, self.canvas, self.new_frame)
-
                 return
             
             elif (Global_height > self.canvas.winfo_height()): # 窗口小于任务栏长度，任务栏可以滑动
                 
-
                 self.Create_New_Frame(None)
                 print("_2")
                 self.update_canvas_height()
 
                 self.canvas.bind_all("<MouseWheel>", lambda event: self.canvas.yview_scroll(-int(event.delta / 60), "units"))
-
-                # printinfo_bigger(Global_height, self.canvas)
 
                 return
         
@@ -291,7 +306,7 @@ class UI:
                 (0, 0), 
                 window=self.new_frame, 
                 anchor="nw", 
-                width=381, 
+                width=self.mainframe.winfo_width() - 19, 
                 height=Global_height
             )
 
@@ -385,46 +400,172 @@ class UI:
                 print("NO DATA CURRENTLY!!!")
 
 
-    def Add_Tasks(self):
+
+    def DeleteJSON(self): # //////////////////// working on /////////////////////////////
+        global data
+        global Tempinfo
+        global sign_set
+
+        Tempinfo = data[:]
+        details_dict_local = Tempinfo[0]
+        tasks_dict_local = Tempinfo[1]
+        details_dict_local.pop(f"Detail_{1}")
+        tasks_dict_local.pop(f"Task_{1}")
+
+
+        with open("data.json", "w", encoding="UTF-8") as file:
+            json.dump(Tempinfo, file, ensure_ascii=False, indent=4)
+        
+        return
+
+        
+
+
+    def Add_Tasks(self) -> dict: # //////////////////// working on /////////////////////////////
         global counter
         global d_conuter
         global Global_height
+        global sign_set
+        
+        # local_sign = {} # define local sign_set 定义 局部标签
+        tasks_sign_list = [] # define local tasks sign_set list 定义 局部任务导入标签
+        details_sign_list = [] # define local details list 定义 局部注解(details)导入标签
 
         print("addtasks d_counter:", d_conuter)
-
-        ## Add Tasks
-
-        # Main Title
-
-        for iter, index in enumerate(range(counter)):
-            
-
-            Global_height += LIST_HEIGHT
-            print("global height:", Global_height)
-
-
-            t = ttk.Label(self.new_frame, text=Tasks_dict[f"Task_{iter}"], font=("", 20))
-            t.pack(anchor="w")
-            # print(iter)
-
-            # print(len(details))
-            # details Text
-            
+        print("counter:", counter)
         
-            t2 = ttk.Label(self.new_frame, text=details[f"Detail_{index}"])
-            t2.pack(anchor="w"); 
-            # print(index)
+        for key in Tasks_dict.keys(): # for 循环 遍历tasks 序号
+            # print("taskskey:", keys)
+
+            tasks_sign_list.append(key)
+
+            # print("tasks_sign_list:", tasks_sign_list)
+            # print("tasks_sign_list_str:", tasks_sign_list[0])
+        for key in details.keys(): # for 循环 遍历details 序号
+            # print("detailskey:", keys)
+
+            details_sign_list.append(key)
+
+            # print("details_sign_list", details_sign_list)
+
+        # relieve memory
+        del key
+
+
+        # set a local counter to iterate
+        LocalCounter = 0 
+        # Get the Max Count of the Tasks
+        MaxTaskCount = max(tasks_sign_list)
+        print("max value:", MaxTaskCount)
+        # Get the actual <n> integer value
+        n = int(MaxTaskCount[5])
+        print("Max tasks count:", n)
+
+        while LocalCounter < len(tasks_sign_list):
+            
+            found = False
+            for i in range(n):  # 检查连续的 <n> 个索引  //////////////////// working on /////////////////////////////
+                if (f"{LocalCounter + i}" in tasks_sign_list[LocalCounter]):
+                    self.Iterate_Tasks(LocalCounter + i)
+                    found = True
+                    break
+
+            if (not found):
+                print("does not exist!!!", "// LocalCounter:", LocalCounter)
+
+            LocalCounter += 1
 
 
 
-            # Finished Button
-            b = ttk.Button(self.new_frame, text="Finished", command=lambda: self.Finished(t, t2, b))
-            b.pack(anchor="e")
 
+
+
+
+
+
+
+
+
+
+            # if (f"{LocalCounter}" in tasks_sign_list[LocalCounter] and f"{LocalCounter}" in details_sign_list[LocalCounter]): # index for tasks and details, "<number>" 用来判断是否存在与 tasks details dictionary
+            #     print("// LocalCounter:", LocalCounter)
+            #     self.Iterate_Tasks(LocalCounter)
+
+
+            # elif (f"{LocalCounter + 1}" in tasks_sign_list[LocalCounter]):
+            #     self.Iterate_Tasks(LocalCounter + 1)
+
+
+            # elif (f"{LocalCounter + 2}" in tasks_sign_list[LocalCounter]):
+            #     self.Iterate_Tasks(LocalCounter + 2)
+
+
+            # elif (f"{LocalCounter + 3}" in tasks_sign_list[LocalCounter]):
+            #     self.Iterate_Tasks(LocalCounter + 3)
+
+
+            # elif (f"{LocalCounter + 4}" in tasks_sign_list[LocalCounter]):
+            #     self.Iterate_Tasks(LocalCounter + 4)
+
+
+            # elif (f"{LocalCounter + n}" in tasks_sign_list[LocalCounter]):
+            #     self.Iterate_Tasks(LocalCounter + n)
+
+                
+            # else:
+            #     print("dosnt exist!!!", "// LocalCounter:", LocalCounter)
+
+            # LocalCounter += 1
+
+
+
+        ## 使用 for 循环 遍历counter 并且 if 判断是否存在于 读取的json文件中 (test)
+        # for iter, index in enumerate(range(counter)):
+        #     if (str(iter) in tasks_sign_list[iter] and str(index) in details_sign_list[index]):
+        #         self.Iterate_Tasks(iter, index)
+        #         # local_sign[sign_set] = f"Task_{iter}"
+        #         # print("loaclsign:", local_sign)
+        #     else:
+        #         print("dosnt exist!!!")
+        
+        # print("localsign:", local_sign)
 
 
         return
     
+
+    ## Add buttons 添加组件 ；每次调用Iterate_Tasks方法时，其中的局部变量t、t2和b都会在不同的内存空间中创建。
+    def Iterate_Tasks(self, iter): # 因为不同的组件被创建在不同的内存空间，所以说需要使用函数为介质来阻挡 for 循环在同一个 局部变量 内生成同一个内存空间的组件
+        global Global_height
+
+        # Add to sign set
+        sign_set.add(iter)
+        print("iter sign_set:", sign_set)
+        sign = list(sign_set)
+    
+        # Update Global_height
+        Global_height += LIST_HEIGHT
+        print("global height:", Global_height)
+
+        # Titles
+        t = ttk.Label(self.new_frame, text=Tasks_dict[f"Task_{iter}"], font=("", 20))
+        t.pack(anchor="w")
+        
+        # Details
+        t2 = ttk.Label(self.new_frame, text=details[f"Detail_{iter}"])
+        t2.pack(anchor="w"); 
+
+
+        # Finished Button
+        b = ttk.Button(self.new_frame, text=f"Finished{sign}", command=lambda: self.Finished(t, t2, b, sign_set))
+        b.pack(anchor="e")
+
+        print("// iter tasks local counter:", iter)
+        
+
+        return 
+        
+
 
 
     
